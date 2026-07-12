6530f84b371d7471ac50d8b2b36372677bb9a97f
Santy sharma <148745658+santysharmaa@users.noreply.github.com>
2026-07-12 16:39:07 +0530

    Time: 1010 ms (5.03%), Space: 23.7 MB (98.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,12 +3,13 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int j = 0;
-
-        for(int i {0}; i<n; i++){
-            if(nums[i] != 0){
-                swap(nums[i], nums[j]);
-                j++;
+        for(int j {0}; j<n; j++){
+            for(int i {0}; i<n-1; i++){
+                if(nums[i] == 0){
+                    int temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = temp;
+                }
             }
         }
         for(int x : nums) cout<<x;
